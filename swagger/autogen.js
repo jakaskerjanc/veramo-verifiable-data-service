7e1ff0a70c5070e0e7812a5fc799b819dffd30d3
jakaskerjanc <100130505+jakaskerjanc@users.noreply.github.com>
2025-03-26 21:34:50 +0100

    Enhance OpenAPI documentation with requestBody definitions and update server configurations

--- swagger/autogen.js
+++ swagger/autogen.js
@@ -1,6 +1,31 @@
 import swaggerAutogen from 'swagger-autogen'
 
+const doc = {
+    "info": {
+      "version": "1.0.0",
+      "title": "REST API",
+      "description": ""
+    },
+    "servers": [
+          {
+              "url": "https://veramo-jrtjw.ondigitalocean.app/",
+              "description": "Public deployment",
+              "variables": {}
+          },
+          {
+              "url": "http://localhost:3000",
+              "description": "Local Development Server",
+              "variables": {}
+          }
+      ],
+    "basePath": "/",
+    "schemes": [
+      "http",
+      "https"
+    ],
+}
+
 const outputFile = 'swagger/openapi.json'
 const endpointsFiles = ['src/index.ts']
 
-swaggerAutogen(outputFile, endpointsFiles)
+swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc)
